8b2155726843d377747bb7cb39bb14126368851b
Paul <110413835+P-EXE@users.noreply.github.com>
2024-02-16 14:52:58 +0100

    Added Status Banner
    Added Status Banner

---
+++ RIPD/Views/Banner/BannerView.xaml.cs
@@ -0,0 +1,9 @@
+namespace RIPD.Views.Banner;
+
+public partial class BannerView : ContentView
+{
+	public BannerView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RIPD/Views/Banner/BannerViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIPD.Views.Banner
+{
+  public partial class BannerViewModel : ObservableObject
+  {
+    [ObservableProperty]
+    string statusText;
+    public string Status => statusText;
+
+    public BannerViewModel()
+    {
+      Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+    }
+
+    private void Connectivity_ConnectivityChanged(object? sender, ConnectivityChangedEventArgs e)
+    {
+      statusText = e.NetworkAccess.ToString();
+    }
+  }
+}
