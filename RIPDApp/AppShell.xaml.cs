36efd27d224e78f6b31cfa21badbb324c7c9474e
Paul <110413835+P-EXE@users.noreply.github.com>
2024-07-07 15:22:17 +0200

    Reorganized the Registering of Services and Routes for the API and App; Added RouteGenerator

--- RIPDApp/AppShell.xaml.cs
+++ RIPDApp/AppShell.xaml.cs
@@ -1,94 +1,9 @@
-﻿using RIPDApp.Pages;
-using RIPDApp.Services;
-
-namespace RIPDApp;
+﻿namespace RIPDApp;
 
 public partial class AppShell : Shell
 {
   public AppShell()
   {
     InitializeComponent();
-
-    #region Routes
-    Routing.RegisterRoute(nameof(AutoLoginPage), typeof(AutoLoginPage));
-    Routing.RegisterRoute(nameof(RegisterPage), typeof(RegisterPage));
-    Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
-
-    Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
-
-    Routing.RegisterRoute(nameof(DiaryTodayPage), typeof(DiaryTodayPage));
-    Routing.RegisterRoute(nameof(DiaryWeekPage), typeof(DiaryWeekPage));
-    Routing.RegisterRoute(nameof(DiaryMonthPage), typeof(DiaryMonthPage));
-
-    #region FoodSearch
-    // Food Search -> Barcode Scanner
-    Routing.RegisterRoute(
-      $"{nameof(FoodSearchPage)}" +
-      $"/{nameof(BarcodeScannerPage)}",
-      typeof(BarcodeScannerPage)
-      );
-    // Food Search -> Food Diary Entry
-    Routing.RegisterRoute(
-      $"{nameof(FoodSearchPage)}" +
-      $"/{nameof(DiaryEntryFoodCreatePage)}",
-      typeof(DiaryEntryFoodCreatePage)
-      );
-    // Food Search -> Food View
-    Routing.RegisterRoute(
-      $"{nameof(FoodSearchPage)}" +
-      $"/{nameof(FoodViewPage)}",
-      typeof(FoodViewPage)
-      );
-    // Food Search -> Food Create
-    Routing.RegisterRoute(
-      $"{nameof(FoodSearchPage)}" +
-      $"/{nameof(FoodCreatePage)}",
-      typeof(FoodCreatePage)
-      );
-    #endregion FoodSearch
-
-    #region FoodDetails
-    // Food Details -> Food Update
-    Routing.RegisterRoute(
-      $"{nameof(FoodSearchPage)}" +
-      $"/{nameof(FoodViewPage)}" +
-      $"/{nameof(FoodUpdatePage)}",
-      typeof(FoodUpdatePage)
-      );
-    // Food Create -> User Search
-    Routing.RegisterRoute(
-      $"{nameof(FoodCreatePage)}" +
-      $"/{nameof(UserSearchPage)}",
-      typeof(UserSearchPage)
-      );
-    // Food Create -> Scan Barcode
-    Routing.RegisterRoute(
-      $"{nameof(FoodCreatePage)}" +
-      $"/{nameof(BarcodeScannerPage)}",
-      typeof(BarcodeScannerPage)
-      );
-    #endregion FoodDetails
-
-    #region WorkoutSearch
-    // Workout Search -> Workout Create
-    Routing.RegisterRoute(
-      $"{nameof(WorkoutSearchPage)}" +
-      $"/{nameof(WorkoutCreatePage)}",
-      typeof(WorkoutCreatePage)
-      );
-    // Workout Search -> Workout Diary Entry
-    Routing.RegisterRoute(
-      $"{nameof(WorkoutSearchPage)}" +
-      $"/{nameof(DiaryEntryWorkoutCreatePage)}",
-      typeof(DiaryEntryWorkoutCreatePage)
-      );
-    #endregion WorkoutSearch
-
-    Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
-    Routing.RegisterRoute(nameof(OwnerProfilePage), typeof(OwnerProfilePage));
-
-    Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
-    Routing.RegisterRoute(nameof(SettingsDevPage), typeof(SettingsDevPage));
-    #endregion Routes
   }
 }
