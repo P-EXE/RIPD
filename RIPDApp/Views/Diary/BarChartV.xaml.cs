2bc74f15e4390ebd517d396b5b6c3c6f58cc44de
Paul <110413835+P-EXE@users.noreply.github.com>
2024-08-12 20:10:24 +0200

    Working on Diary Pages

--- RIPDApp/Views/Diary/BarChartV.xaml.cs
+++ RIPDApp/Views/Diary/BarChartV.xaml.cs
@@ -1,9 +1,18 @@
+using RIPDShared.Models;
+using System.Collections.ObjectModel;
+
 namespace RIPDApp.Views;
 
 public partial class BarChartV : ContentView
 {
-	public BarChartV()
-	{
-		InitializeComponent();
-	}
+  public static readonly BindableProperty ValuesProperty = BindableProperty.Create(nameof(Values), typeof(ObservableCollection<double>), typeof(BarChartV));
+  public ObservableCollection<double> Values
+  {
+    get => (ObservableCollection<double>)GetValue(ValuesProperty);
+    set => SetValue(ValuesProperty, value);
+  }
+  public BarChartV()
+  {
+    InitializeComponent();
+  }
 }
