efc4423eb44a5f1e5709031d2db6abcb22a989a8
Paul <110413835+P-EXE@users.noreply.github.com>
2024-08-21 10:01:56 +0200

    Separated startup config

---
+++ RIPDApi/Config/ControllerConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RIPDApi.Config;
+
+public static class ControllerConfig
+{
+  public static async Task RegisterControllers(this IServiceCollection services)
+  {
+    IMvcBuilder builder = services.AddControllers();
+    await ConfigureJsonOptions(builder);
+  }
+
+  private static Task ConfigureJsonOptions(IMvcBuilder builder)
+  {
+    builder.AddJsonOptions(options =>
+    {
+      options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+      options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+      options.JsonSerializerOptions.WriteIndented = true;
+    });
+    return Task.CompletedTask;
+  }
+}
---
+++ RIPDApi/Config/RepoConfig.cs
@@ -0,0 +1,16 @@
+﻿using RIPDApi.Repos;
+
+namespace RIPDApi.Config;
+
+public static class RepoConfig
+{
+  public static Task RegisterRepos(this IServiceCollection services)
+  {
+    services.AddTransient<IFoodRepo, FoodRepo>();
+    services.AddTransient<IWorkoutRepo, WorkoutRepo>();
+    services.AddTransient<IDiaryRepo, DiaryRepo>();
+    services.AddTransient<IUserRepo, UserRepo>();
+
+    return Task.CompletedTask;
+  }
+}
