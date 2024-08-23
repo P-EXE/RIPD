4dc8e3f354d15c2aab2d58b30f450ecbf2735654
Florian <fh@bruchpilot.at>
2024-08-23 13:28:23 +0200

    Charts updated

--- RIPDApp/Pages/Diary/DiaryMonthPage.xaml.cs
+++ RIPDApp/Pages/Diary/DiaryMonthPage.xaml.cs
@@ -1,9 +1,14 @@
+using RIPDApp.ViewModels;
+
 namespace RIPDApp.Pages;
 
 public partial class DiaryMonthPage : ContentPage
 {
-	public DiaryMonthPage()
+	private readonly DiaryVM _vm;
+	public DiaryMonthPage(DiaryVM vm)
 	{
 		InitializeComponent();
+		_vm = vm;
+		BindingContext = _vm;
 	}
 }
